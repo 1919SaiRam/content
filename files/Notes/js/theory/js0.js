2b248000716283e018018be6feffcfb6f6fd55d9
Chinta Vijay Kumar <vijaykumarchinta1919@gmail.com>
2025-01-12 21:38:51 +0530

    Update js0.js

---
+++ files/Notes/js/theory/js0.js
@@ -0,0 +1,12 @@
+Understand programming concepts through practical activities
+Sequence
+Selection
+Iteration
+Data types
+Variables
+Lists
+Debugging
+Functions
+Use combination of these concepts to solve  problems.
+Basic introduction to Javascript.
+Programming is not difficult, if you have a strategy and practice :)
