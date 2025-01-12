159d2bba1a8983eefbe6ea2b8d548f3d4a64b791
Chinta Vijay Kumar <vijaykumarchinta1919@gmail.com>
2025-01-12 21:53:31 +0530

    Update js0.js

--- files/Notes/js/theory/js0.js
+++ files/Notes/js/theory/js0.js
@@ -1,5 +1,5 @@
 #Understand programming concepts through practical activities
-Sequence
+Sequence ; Algorithm ; Program
 Selection
 Iteration
 Data types
@@ -16,3 +16,16 @@ Basic introduction to Javascript.
 Sequence is part of an algorithm.
 Sequence is the order in which the instructions are executed.
 While Algorithm is the sequence of instructions to achieve a task. 
+When an Algorithm is written in a programming language we call it as Program.
+There are many Programming languages like JavaScript, Java, Python, C++, Blockly etc.
+
+An integrated development environment (IDE) is a software application that helps programmers write, compile, and debug code more efficiently. IDEs are designed to support specific programming languages or related languages. 
+Here are some features of IDEs:
+Editors: Provide tools for writing and editing code, such as automatic line numbering, color coding, auto-correct, auto-suggestion, and auto-indent 
+Build automation: Automate tasks like compiling source code into binary code, packaging binary code, and running automated tests 
+Debuggers: Test other programs and graphically display the location of bugs in the original code 
+Version control: Track and manage changes to code 
+Stepping: Monitor the effect of each line of code by executing a single line at a time 
+Error message list: Display errors, warnings, and messages generated during compilation and execution 
+Autocomplete: Anticipate what the programmer is going to type next 
+IDEs can help programmers focus on logic in their code instead of learning about and manually integrating different software tools.  
