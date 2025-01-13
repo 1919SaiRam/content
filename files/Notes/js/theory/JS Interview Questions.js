ad89dd9d6b997233d79c7b71d14316a5d67fa98d
Chinta Vijay Kumar <vijaykumarchinta1919@gmail.com>
2025-01-13 18:12:41 +0530

    Update and rename js1.js to JS Interview Questions.js

---
+++ files/Notes/js/theory/JS Interview Questions.js
@@ -0,0 +1,89 @@
+Q1: What's the difference between undefined and null?
+
+
+Q2: What is the output of the following code? Explain
+ let num;
+ console.log(typeof(typeof(x)));
+1)number    2)string   3)boolean  4)undefined
+
+
+
+Q3:   What is the output of the following code? Explain
+console.log(typeof(null));
+1)number    2)string   3)boolean  4)OBJECT
+
+
+
+Q4:   Which line number will throw an error:
+let a = 20;  // line #1
+var b = 10;  // line #2
+let a = 40;  // line #3
+var b = 50;  // line #4
+const c = 60 // line #5
+a .line #2        b. line #3           c. line #4          d. line #5
+
+
+
+Q5: In JavaScript, what does x===y statement implies ?
+
+
+  
+Q6:  What is the output of the following code? Explain
+console.log(1 === 1);
+console.log([1, 2] === [1, 2])
+a.  true true     b.  false false    c.  true false      d.  false true
+
+
+Q7: What are truthy and falsy values?
+
+
+  
+Q8: What is the output of the following code? Explain
+  if ('') {
+      console.log("I am true");
+   } else {
+      console.log("I am false");
+   }
+a.  I am true       b.  I am false      c.  Error           d.  None of the above
+
+
+
+Q9: What is the output of the following code? Explain
+ console.log("50" + 50 - 50)
+a.  50           b.  500        c.  5000              d.  50000
+
+  
+  
+Q10: What is the output of the following code? Explain
+let str = "Kevin";
+str[0] = 'R';
+console.log(str);
+a.  Kevin         b.  Revin         c.   R            d.  Error
+
+
+
+Q11: What is the output of the following code? Explain
+let arr = [ 4,30,2,10];
+console.log(arr.sort())
+a.  [2, 4, 10, 30]          b.  [10, 2, 30, 4]                    c.  [10, 30, 2, 4]                         d.  [30, 10, 4, 2]
+
+
+
+Q12: What is the output of the following code? Explain
+let a = [1, 2, 3, 4, 5];
+console.log(a.slice(2,4));
+console.log(a.splice(2,4));
+a.  [ 3, 4 ] & [ 3, 4, 5 ]           b.  [ 3, 4 ] & [ 3, 4]              c.  [ 2, 3 ] & [ 2,4, 5]              d.  [ 3, 4, 5 ] & [ 3, 4]
+
+
+Q13: What is Closure?
+
+Q14: What is Hoisting?
+
+
+
+
+
+  
+
+  
