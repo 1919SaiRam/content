96656b923ec3908066b5f030b9d914283171bd50
Chinta Vijay Kumar <vijaykumarchinta1919@gmail.com>
2025-01-13 06:50:14 +0530

    Update js0.js

--- files/Notes/js/theory/js0.js
+++ files/Notes/js/theory/js0.js
@@ -1,11 +1,12 @@
 #Understand programming concepts through practical activities
 Sequence ; Algorithm ; Program
+Debugging
+Binary Language
 Selection
 Iteration
 Data types
 Variables
 Lists
-Debugging
 Functions
 Use combination of these concepts to solve  problems.
 Basic introduction to Javascript.
@@ -18,5 +19,14 @@ Sequence is the order in which the instructions are executed.
 While Algorithm is the sequence of instructions to achieve a task. 
 When an Algorithm is written in a programming language we call it as Program.
 There are many Programming languages like JavaScript, Java, Python, C++, Blockly etc.
+Computer programs are algorithms written in programming languages that the computer can understand.
+
+Debugging :- 
+Errors are the problems or faults that occur in a program. These make the behavior of the program abnormal.
+Programming errors are also known as the bugs. The process of finding & fixing these bugs is called debugging.
+  
+  programming languages that are known as high-level languages
+  The language of 0s and 1s, which is called Binary Language 
+
 
 
