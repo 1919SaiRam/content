499741ad39a335241e26ffede2b10b99c3ca3efa
Chinta Vijay Kumar <vijaykumarchinta1919@gmail.com>
2025-01-13 07:06:38 +0530

    Update js0.js

--- files/Notes/js/theory/js0.js
+++ files/Notes/js/theory/js0.js
@@ -2,10 +2,15 @@
 Sequence ; Algorithm ; Program
 Debugging
 Binary Language
-Selection
-Iteration
+Compiler
+Keywords
 Data types
 Variables
+Arithmetic Operators
+Events
+
+Selection
+Iteration
 Lists
 Functions
 Use combination of these concepts to solve  problems.
@@ -28,5 +33,40 @@ Programming errors are also known as the bugs. The process of finding & fixing t
   programming languages that are known as high-level languages
   The language of 0s and 1s, which is called Binary Language 
 
+Compiler
+The language processor that reads the program written in high-level language and translates it into an equivalent program in machine language is called a Compiler.  
+
+  Keywords are reserved words used by the compiler. It tells the compiler that the keyword has a specific meaning AND it cannot be used for another purpose.
+
+  
+# Data Types in Javascript
+
+String : A string is a series of characters like "Michael Jackson".
+Strings are written with quotes. You can use single or double quotes. Example : “Crio”, ‘crio’.
+
+Number : All JavaScript numbers are stored as decimal numbers (floating point).
+Numbers can be written with, or without decimals. Example : 34.00, 34
+
+Boolean : Booleans can only have two values: true or false. 
+For example: When you select Dark Mode or Light Mode on browser, the browser remembers it as Dark mode = True.
+
+  
+# Data types in Java
+
+string: A string is a series of characters like "Michael Jackson".
+Strings are written with quotes. You can use single or double quotes. Example : “Crio”, ‘crio’.
+
+int: The integer data type represents a positive whole number or its negative value
+Examples : -9834, 0,1, 87
+
+float: It represents floating-point or decimal number.
+Example : 0.1234, 12.245, -2324.0
+
+boolean : booleans can only have two values: true or false. 
+For example, when you select Dark Mode or Light Mode on browser, the browser remembers it as Dark mode = True.
 
 
+Variables to store and access data when a program is executing. 
+
+
+  
