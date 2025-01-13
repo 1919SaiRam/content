409ade55baa978c1535c838342e5f418bedc503d
Chinta Vijay Kumar <vijaykumarchinta1919@gmail.com>
2025-01-13 18:46:02 +0530

    Update DSA..js

--- files/Notes/js/theory/DSA..js
+++ files/Notes/js/theory/DSA..js
@@ -1,1 +1,88 @@
+DSA (What? Why? How?)
+How to master DSA?
+Programming fundamentals
+Crio DSA platform
+Recap - Variables, syntax, data types, loops, conditional statements
+Introduction to Space and Time Complexity
+What is Time Complexity?
+What is Space Complexity?
+Tradeoff between the two
+
+
+------------------------=================--------------------================-------------------------------------------------------
+DSA :-
+  
+What?
+Combination of Data Structures and Algorithms
+Data structure: efficient data management to improve storage and retrieval
+Algorithm: set of instructions to solve a particular problem
+
+Why should you learn?
+A building block of Software Development
+Helps us keep our applications/softwares memory and time efficient
+Helps shape our thought process - try to optimize our solution, suggest alternatives
+Similar process is being followed in Software Development
+Popular interview standard
+
+How?
+Consistency is the key, practice daily!!
+Ensure that you solve problem on your own, do not copy-paste from internet
+Ok to read/understand the solution, but please implement it on your own after that
+We will share a methodology (a step by step process) that can be used to solve any problem
+Avoid procrastination and stacking up the problems for the last week
+Discuss your ideas with mentors/TAs/peers
+  
+
+----------->>>>>>>       Programming fundamentals
+#Variables
+Containers for storing data values
+
+#Syntax
+Each programming language has it own syntax (though there are a lot of similarity)
+Get comfortable with the fundamentals
+
+#Keywords: predefined, reserved words that have special meanings to the compiler
+
+#Data types
+Classification of data like: int, char, float, boolean
+
+Further sub-division of data. 
+Why do you think this sub classification is required? - Better memory management
+
+Data types vs data structures?
+  
+#Constraints
+Refers to restrictions, limitations.
+Generally imposed on the input, helps us figure out the correct data type and algorithm
+Example: the array given to you will have less than 10^6 elements
+
+Debugging
+Identification and removal of bugs/errors in the code
+Important skill that comes when writing and executing code
+
+------------->>>>>>>>>>> Programming Constructs
+
+Conditionals: Helps in decision making 
+If
+switch
+break
+continue
+Loops: Helps in iterating over a range
+for
+while
+do while
+
+If Else Block
+Nested if else
+Switch
+
+Loops
+Selection of a loop is always a tough task for a programmer, to select a loop do the following steps:
+Analyze the problem and check whether it requires a pre-test or a post-test loop.
+If pre-test is required, use a while or a for loop.
+If post-test is required, use a do-while loop.
+
+
+
+
 
