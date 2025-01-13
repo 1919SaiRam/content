7ce98216a58468e4b573206102c3e91efbc138aa
Chinta Vijay Kumar <vijaykumarchinta1919@gmail.com>
2025-01-13 18:27:06 +0530

    Update JS Interview Questions.js

--- files/Notes/js/theory/JS Interview Questions.js
+++ files/Notes/js/theory/JS Interview Questions.js
@@ -80,10 +80,157 @@ Q13: What is Closure?
 
 Q14: What is Hoisting?
 
-
-
-
-
   
+Q15:What is the output of the following code? Explain
+  function add(){
+    console.log(answer)
+    var answer = 2
+};
+add();
+a.  2           b.  Error             c.  undefined                    d.  None of the above
 
-  
+
+
+Q16: Is this statement true or false? 
+var is a function-based scope
+
+
+Q17:What is the lexical scope?
+
+
+Q18:What is the output of the following code? Explain
+function add(obj){
+  	obj.id = obj.id + 1;
+}
+var obj = {
+     id: 9
+};
+add(obj)
+console.log (obj.id);
+a.  9                   b.  10                c.  Error                          d.  None of the above
+
+
+Q19: What is the output of the following code? Explain
+const obj = {
+    	    id: 9
+};
+const temp = obj;
+temp.id = 25;
+console.log(obj.id);
+a.  9                   b.  25               c.  Error                          d.  None of the above
+
+
+Q20: What is the output of the following code? Explain
+  const obj = {
+        name: ‘Edwin’
+};
+const temp = {...obj};
+temp.name= ‘Kevin’;
+console.log(obj.name);
+a. Edwin                    b.Kevin                 c.  Error                          d.  None of the above
+
+
+Q21:  What is the output of the following code? Explain
+  let customers = [
+  {
+    name: 'Rocky',
+    credit: 100
+  },
+  {
+    name: 'Tuffel',
+    credit: 200
+  },
+  {
+    name: 'Gigi',
+    credit: 300
+  }
+];
+console.log(customers.find(c => c.credit > 100));
+A.   { name: 'Tuffel', credit: 200 }            B.    { name: 'Gigi', credit: 300 }                  C.     Both A & B                    D.     None of the above
+
+
+
+Q22:  What is the output of the following code? Explain
+let emp = [
+    {
+        "id": 1,
+        "name":"Randy",
+        "salary":75000
+    },
+    {
+        "id": 2,
+        "name":"Kevin",
+        "salary":60000
+    },
+    {
+        "id": 3,
+        "name":"Ceri",
+        "salary":30000
+    }]
+let out = emp.filter(emp => emp.salary > 40000)
+console.log(out);
+a.   [ { id: 1, name: 'Randy', salary: 75000 },
+    { id: 2, name: 'Kevin', salary: 60000 } ]                             b.  [ { id: 2, name: 'Kevin', salary: 60000  }]           C.   [ { id: 1, name: 'Randy', salary: 75000 }]          d.  None of the above
+
+
+
+Q23:  What is the output of the following code? Explain
+let emp= [
+    {
+        "id": 1,
+        "name":"Randy",
+        "salary":75000
+    },
+    {
+        "id": 2,
+        "name":"Kevin",
+        "salary":60000
+    },
+    {
+        "id": 3,
+        "name":"Ceri",
+        "salary":30000
+    }]
+let out = emp.reduce((a,empl)=> a + empl.salary, 0)
+console.log(out);
+
+
+Q24:  What is the output of the following code? Explain
+let text = "Good words without deeds are just words";
+console.log(text.lastIndexOf("words") - text.indexOf("words"));
+
+// Given that 
+text.lastIndexOf("words") =  34
+
+
+Q25:  What is the output of the following code? Explain
+function solve(arr, rotate){
+ for(let i = 0; i < rotate; i++){
+   let element = arr.pop();
+   arr.unshift(element);
+ }
+ return arr;
+}
+console.log(solve([44, 1, 22, 111], 3));
+
+
+
+Q26:  What is the output of the following code? Explain
+console.log (Math.abs(Math.floor(-20.5)));
+
+
+
+Q27:  Is the below conversion of a simple function to an arrow function correct?
+// Simple Function
+function add (x,y){
+  return x+y;
+}
+console.log(add(5,2));
+
+// Arrow Function
+let add = (x,y) => x + y;
+console.log (add(5,2));
+
+
+
+
