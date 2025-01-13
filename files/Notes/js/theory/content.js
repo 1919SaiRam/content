5254a298e606ca7275f343d9f15b93c78b9213f6
Chinta Vijay Kumar <vijaykumarchinta1919@gmail.com>
2025-01-13 06:39:51 +0530

    Update content.js

---
+++ files/Notes/js/theory/content.js
@@ -0,0 +1,10 @@
+An integrated development environment (IDE) is a software application that helps programmers write, compile, and debug code more efficiently. IDEs are designed to support specific programming languages or related languages. 
+Here are some features of IDEs:
+Editors: Provide tools for writing and editing code, such as automatic line numbering, color coding, auto-correct, auto-suggestion, and auto-indent 
+Build automation: Automate tasks like compiling source code into binary code, packaging binary code, and running automated tests 
+Debuggers: Test other programs and graphically display the location of bugs in the original code 
+Version control: Track and manage changes to code 
+Stepping: Monitor the effect of each line of code by executing a single line at a time 
+Error message list: Display errors, warnings, and messages generated during compilation and execution 
+Autocomplete: Anticipate what the programmer is going to type next 
+IDEs can help programmers focus on logic in their code instead of learning about and manually integrating different software tools.  
