8f3a2caa80fb88e55458ed8528a6acf14542344a
Chinta Vijay Kumar <vijaykumarchinta1919@gmail.com>
2025-01-14 15:22:56 +0530

    Update DsadsaDsadsa.js

--- files/Notes/js/theory/DsadsaDsadsa.js
+++ files/Notes/js/theory/DsadsaDsadsa.js
@@ -5,6 +5,9 @@ Heapify
 Build Heap
 
 
+Introduction to Greedy Algorithms
+Introduction to Recursion
+Introduction to Backtracking
 
 
 
@@ -239,4 +242,271 @@ Minimum Platforms
 Gas Stations
 
   
+Introduction to Recursion
+Recursion is applicable to problems which can be split into similar subproblems and we can solve each subproblem to solve the large problem. 
+This would see the function being invoked from within itself, making it a recursive function.
+In some cases, recursion is an easy and more comprehensible way to solve a problem.
+Popular uses: Merge Sort, Quick Sort, Binary Search, Depth First Search/Breadth First Search
+How does recursion work?
+The first function call creates an instance of the function/method on the Call Stack. Every time there is a recursive call, this instance gets paused (its state is maintained) and a new instance gets added to the top of the Call Stack.
+When the terminating condition is met and an instance of the method returns, that instance is popped off the Call Stack and the next instance gets the returned value. This instance in turn returns and gets popped and so on. Finally, the first instance on the Call Stack gets the returned value and finishes up the solution, clearing the Call Stack.
+
+Some important points
+Recursion is a method of defining a function in terms of itself
+ Example: the Fibonacci numbers   f (n) = f(n-1) + f(n-2) • f(0) = f(1) = 1 
+In programming, recursion is a method call to the same method. In other words, a recursive method is one that calls itself.
+Recursion is a good problem solving approach:
+Where we solve a problem by reducing the problem to smaller subproblems; this results in recursive calls.
+Recursive algorithms are elegant, simple to understand and prove to be correct, also easy to implement 
+But! Recursive calls can result in a an infinite loop of calls if terminating conditions are not set correctly. Recursion needs a proper base-case in order to stop.
+
+  Key Aspects
+Terminating Condition
+This is the condition that will result in a recursive instance of the method doing a return. Without this, the recursion might go on till there is no space on the Call Stack. It is important that this condition is met for the solution to be complete.
+Recursive Invocation Condition
+Based on the goal of the problem, the function gets recursively invoked with reducing input parameters, till the terminating condition is met.
+Recursion Depth
+This is something to keep an eye out for. If the number of instances on the Call Stack exceeds the maximum number supported for that language, you will see a “Maximum call stack size exceeded” exception.
+
+  Approach 
+ To solve a problem recursively 
+Break into smaller 
+problems 
+Solve sub-problems 
+recursively 
+Assemble sub-solutions
+
+Example: Write a function that computes the sum of numbers from 1 to n
+Using a loop 
+Using Recursion
+
+Visualize
+
+Example - Fibonacci
+Find the nth Fibonacci number
+Fibonacci series - 0,1,1,2,3,5,8,13...
+Terminating condition: When n is 0 or 1, return it 
+Recursive invocation condition: When n is > 1
+
+What does the iterative solution look like?
+int fibonacciIterative(int n) {
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    int prev1 = 0, prev2 = 1, nth = 1;
+    for (int i = 2; i <= n; i++) {
+        nth = prev1 + prev2;
+        prev1 = prev2;
+        prev2 = nth;
+    }
+    return nth;
+}
+
+int fibonacciRecursive(int n) {    
+    if (n <= 1) {      // Termination
+        return n;
+    } else {              // Recursion
+        return (fibonacciRecursive(n-1) + fibonacciRecursive(n-2))
+    }
+}
+
+
+Additional Information
+Recursive solutions are not space efficient compared to iterative solutions, since the problems need stack space. All recursive solutions can be implemented iteratively, but the logic may be complex. Take a call between these trade-offs.
+The Recursive solution may also not be time efficient compared to the iterative solution
+For example, in the fibonacci series, while calculating Fib (5), we’ll calculate Fib (2) three times. This repeated calculation gets far worse as the input number increases.
+Time complexity of Recursive solutions can be improved with Memoization or Dynamic Programming. We’ll visit this later.
+Visualise recursion as a stack when you’re trying to solve problems
+More applicable to these data structures -  trees, graphs, heaps, linked lists
+Applicable to these algorithms - memoization, backtracking
+
+Optional Activity - Find the nth Tribonacci number
+Given a number N, find the Nth Tribonacci number
+A Tribonacci Series is a series where each number in a sum of the previous 3 numbers in the sequence.
+0, 1, 1, 2, 4, 7, 13, 24, 44, 81 …
+Note that 0 is the 0th number, not the 1st number here.
+
+Example 1
+Input: N = 6
+Output: 13
+Explanation: 7 is the 6th element in the Tribonacci series
+
+Example 2
+Input: N = 25
+Output: 1389537
+Explanation: The 25th number in the Tribonacci sequence is 1389537
+
+Problem - Iterative Approach
+Iterative approach involves using 3 variables to keep track of the last 3 numbers and repeating the sum calculation n times.
+
+public int tribonacciIterative(int n) {
+    if (n < 3) 
+        return n == 0 ? 0 : 1;
+
+    int first = 0, second = 1, third = 1, temp = 0;    
+    for (int i = 3; i <= n; ++i) {
+        temp = first + second + third;
+        first = second;
+        second = third;, 
+        third = temp;
+    }
+    return third;
+}
+Time Complexity: O(N), we traverse through all numbers till N.
+Space Complexity: O(1), constant space used for the variables.
+
+
+  Problem - Recursive Approach
+Can we use Recursion to solve this problem?
+Yes, since it can be broken down into subproblems of calculating the 3 previous numbers, which in-turn need their 3 previous numbers and so on.
+Terminating condition - if n < 3
+Recursive invocation condition - if n >= 3
+This recursive solution is very slow due to the huge number of repeated calculations needed for larger numbers.
+  
+int tribonacciRecursive(int n) {    
+    if (n < 3) {     // Termination
+        return n == 0 ? 0 : 1;
+    }
+    else {             // Recursion
+        return (tribonacciRecursive(n-1) + 
+                       tribonacciRecursive(n-2) +
+                       tribonacciRecursive(n-3));
+    }
+}
+
+Time Complexity: O(3^N), which is exponential!
+Space Complexity: O(N), for the recursive stack which could go to a maximum depth of N.
+
+Alternate approaches
+This can be optimized with Memoization or Dynamic Programming, which we’ll cover under a later concept.
+
+
+  Takeaways
+When to use?#
+When the problem can be broken down into similar smaller subproblems and solving subproblems can lead to overall solution
+When the problem becomes very complex when done iteratively (due to nested loops)
+
+How to use?
+Start with the smallest input, how do we solve it? 
+Increase the input size by 1 and then by 2 and see if a pattern emerges.
+Design the logic to break the problem down into recursive call(s) with reducing input parameters
+Write the terminating condition where the function will start returning - usually when the reducing input parameter is sufficiently small
+
+
+
+Frequently asked problems
+Find Factorial of a number using recursion
+Find the Kth permutation
+Integer division without using * or /
+All possible combinations for a given sum
+Calculate power of a given number using recursion
+Towers of Hanoi
+Calculate the nth fibonacci number using recursion
+Recursive method to generate all permutations of a string
+Sum of integers from 1 to n using recursion
+Calculate modulo using recursion
+GCD of two numbers using recursion
+Check if a number is prime using recursion
+Convert decimal number to binary using recursion
+Reverse a string using recursion
+Remove duplicate letters from string
+Find if a string a palindrome using recursion
+Invert the position of elements in an Array
+Find length of a linked list using recursion
+Print the reversed linked list using recursion
+Search a linked list for a number
+
+
+Activity 1 - Tower of Hanoi
+Tower of Hanoi is a puzzle where we have three rods(A,B,C) and N disks. All the disks are of distinct size and initially they are placed on rod A in sorted order of their size with the largest at the bottom and the smallest at the top. Rods B and C are initially empty. The objective is to move the entire stack from A to rod C with the help of rod B, following these rules:
+Only one disk can be moved at a time.
+Each move consists of taking the upper disk from one of the stacks and placing it on top of another stack i.e. a disk can only be moved if it is the uppermost disk on a stack.
+Larger disk cannot be placed on top of a smaller disk.
+Disk are numbered from 1 to N where 1 is the smallest disk and N is the largest.
+Print all the moves made to get the disks from A to C.
+
+  Recursive Approach
+Iterative approach can get very complex. Refer https://en.wikipedia.org/wiki/Tower_of_Hanoi#Iterative_solution
+Intuitively, this problem can be broken down into similar smaller subproblems. Below is the approach we can use to solve this. Consider the case where N = 2 and visualize this:
+Shift N-1 disks from A to B
+Shift last disk from A to C   
+Shift N-1 disks from B to C  (Here B and A have switched places. A can act as the auxiliary rod)
+Let’s also visualize this with N = 3.
+Terminating condition: 1 disk to be moved from Source to Destination
+Recursive invocation condition: Any number of disks > 1
+
+How do we get to the Recursive Approach?
+Start with the smallest case i.e. N = 1
+Move Disk1 from A to C
+
+When N = 2
+Move Disk1 from A to B (B is the auxiliary rod that holds Disk1 while Disk2 is moved from A to C)
+Move Disk2 from A to C
+Move Disk1 from B to C
+
+When N = 3
+Move Disk1 and Disk2 from A to B. From N = 2 case, we know that we can move 2 Disks between 2 rods using a third auxiliary rod. 
+Move Disk3 from A to C
+Move Disk1 and Disk2 from B to C. We know how to do this from N = 2 case. Use A as the auxiliary rod here.
+
+When N = 4
+Move Disk1, Disk2 and Disk3 from A to B. From N = 3 case, we know how to do this.
+Move Disk4 from A to C
+Move Disk1, Disk2 and Disk3 from B to C. From N = 3 case, we know how to do this.
+
+And so on...
+
+
+  Recursive Solution
+// Start with towersOfHanoi(int n, 'A', 'C', 'B')
+static void towersOfHanoi(int n, char From, char To, char Aux) {
+    if (n == 1)  {     // Terminating condition
+        System.out.println("1 " + From + " " + To);
+        return;
+    }
+    towersOfHanoi(n-1, From, Aux, To);
+    System.out.println(n + " " + From + " " + To);
+    towersOfHanoi(n-1, Aux, To, From);
+}
+
+Time Complexity: O(2^N), which is exponential. 
+Space Complexity: O(N), for the recursive stack which could go to a maximum depth of N.
+
+
+  Activity 2 - Letter combinations of Phone Number
+Given a string str, containing digits from 2 - 9 inclusive, write a program to return all the possible letter combinations that the number could represent.
+Input: digits = "23"
+Output: [ad","ae","af","bd","be","bf","cd","ce","cf"]
+
+
+Analysis
+In this question, we need to find all the combinations of letters which can be obtained if a number is pressed. For e.g., from the above image 3 represents def and 5 represents jkl so if someone were to press 35, they will get one of the following combinations —
+dj, dk, dl, ej, ek, el, fj, fk, fl
+We need to find these combinations. If we look at it more closely, there are a few things to notice —
+We need to take care of only numbers 2,3,4,5,6,7,8,9.
+There can be a maximum of 4 characters in a given string.
+The result needs to be the Cartesian product of the given letters’ combination.
+
+How do we get to the Recursive Approach?
+If you are thinking about recursion, then you are on the right track. At each stage of recursion there will be a digit that will represent some characters, and then we will recursively send the next digit to look for those set of characters that could be appended in our result string.
+
+Create a recursive function recurse(combination, next_digits)
+Add a base case: If there are no more digits to check that means that the current combination is done.
+If there are still digits to check :
+Iterate over the letters mapping the next available digit.
+Append the current letter to the current combination combination = combination + letter.
+Proceed to check the next digits : recurse(combination + letter, next_digits[1:]).
+
+
+  Visualise
+
+Questions?
+Generate parentheses
+Find combinations that sum to a target
+
+
+
+
+
+Introduction to Backtracking
+
 
