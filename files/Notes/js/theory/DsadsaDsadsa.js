a3c15c3b8dce2a19be7460fac92003a3be737a8e
Chinta Vijay Kumar <vijaykumarchinta1919@gmail.com>
2025-01-14 14:28:04 +0530

    Create DsadsaDsadsa.js

---
+++ files/Notes/js/theory/DsadsaDsadsa.js
@@ -0,0 +1,21 @@
+Heaps
+Concept Introduction
+Insertion and Deletion
+Heapify
+Build Heap
+
+
+
+
+
+-----------------------------------===============================--------------------------==================================------------------------------------
+
+  Introduction to Heaps
+Heaps are the data structure that can access the maximum or minimum element very quickly
+They are implemented using arrays (can also use linked lists). The Array Map of a Heap would like like this
+You will notice that the heap can be visualized as a nearly complete binary tree.
+Array Map
+Root of the tree is the first element in the array corresponding to i = 1
+The parent of node i = i / 2 → Index of parent node
+Left child of node i = 2 * i → Index of left child
+Right child of node i = 2 * i + 1 → Index of right child 
